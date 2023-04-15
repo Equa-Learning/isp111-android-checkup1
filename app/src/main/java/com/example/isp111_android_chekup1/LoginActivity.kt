e08a8dca467b16e8d94504e43665f023f58508bf
equa <k.ermolin@knopka.com>
2023-04-15 18:10:38 +0500

    login activity

--- app/src/main/java/com/example/isp111_android_chekup1/LoginActivity.kt
+++ app/src/main/java/com/example/isp111_android_chekup1/LoginActivity.kt
@@ -1,22 +1,22 @@
 package com.example.isp111_android_chekup1
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 
     fun toLogin(view: View) {
-        var intent = Intent(this@MainActivity, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+
     }
     fun toRegistragion(view: View) {
 
     }
+
+    fun toRecoverPassword(view: View) {}
+    fun doLogin(view: View) {}
 }
